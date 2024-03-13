dc191adf670ecf067e17f3906a69e6ae39e27a04
abhijit-003 <abhijitchavan005@gmail.com>
2024-03-13 11:45:24 +0530

    commit

--- Java/demo.java
+++ Java/demo.java
@@ -1,7 +1,8 @@
 package Java;
 
 public class demo {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("hello, world");
+        System.out.println("hello");
     }
 }
