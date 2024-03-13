c9dc4499cf1bbac96ec506394fd7f1c505504745
Abhijit <129248907+abhijit-003@users.noreply.github.com>
2024-03-13 12:49:58 +0530

    374

---
+++ Java/Guess Number Higher or Lower.java
@@ -0,0 +1,29 @@
+//374
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int start = 1;
+        int end = n;
+        if(guess(start) == 0) return start;
+        if(guess(end) == 0) return end;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            int num = guess(mid);
+            if(num == 0)
+                return mid;
+            else if(num == -1)
+                end = mid-1;
+            else 
+                start = mid+1;
+        }
+        return 0;
+    }
+}
