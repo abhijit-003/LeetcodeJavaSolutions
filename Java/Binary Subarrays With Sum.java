8dad2267f3a6234138ee2f82103ccd2b0f65d639
Abhijit <129248907+abhijit-003@users.noreply.github.com>
2024-03-14 14:42:21 +0530

    930

---
+++ Java/Binary Subarrays With Sum.java
@@ -0,0 +1,30 @@
+//930
+class Solution {
+    //sliding window solution
+    private static int countSubArray(int nums[],int goal){
+        int len = nums.length;
+        int lowerIndex = 0;
+        int higherIndex = 0;
+        int sum = 0;
+        int count = 0;
+
+        if(goal<0)
+            return 0;
+
+        while(higherIndex<len){
+            sum += nums[higherIndex];
+            while(sum>goal){
+                sum-=nums[lowerIndex];
+                lowerIndex++;
+            }
+
+            count += (higherIndex-lowerIndex+1);
+            higherIndex++;
+        }
+        return count;
+    }
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int noOfSubarrayWithSum = countSubArray(nums,goal) - countSubArray(nums,goal-1);
+        return noOfSubarrayWithSum;
+    }
+}
