1d2314b5911f259b6210a6c5a0fcbaf533fa42ef
Abhijit <129248907+abhijit-003@users.noreply.github.com>
2024-03-15 19:23:48 +0530

    Create Determine if a Cell Is Reachable at a Given Time.java

---
+++ Java/Determine if a Cell Is Reachable at a Given Time.java
@@ -0,0 +1,13 @@
+//2849 Determine if a Cell Is Reachable at a Given Time
+class Solution {
+    public boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        int xDist = Math.abs(sx - fx);
+        int yDist = Math.abs(sy - fy);
+
+        if (xDist == 0 && yDist == 0) {
+            return t != 1;
+        }
+
+        return xDist <= t && yDist <= t;
+    }
+}
